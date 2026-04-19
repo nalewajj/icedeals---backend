e3c7d5ef03f7352a492649879ccb7e0e25cd2aed
nalewajj <damiannalewaj@gmail.com>
2026-04-19 18:12:21 +0200

    Add authentication and deals routes to the server

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
-
 import express from "express";
 import cors from "cors";
+import authRoutes from "./routes/auth.js";
+import dealRoutes from "./routes/deals.js";
 
 const app = express();
 app.use(cors());
 app.use(express.json());
+
+app.use("/auth", authRoutes);
+app.use("/deals", dealRoutes);
 
 app.get("/", (req, res) => {
   res.send("IceDeals backend działa");
