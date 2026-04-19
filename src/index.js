8ca033b5cb240403bff5969b2b016cb2b56b8512
nalewajj <damiannalewaj@gmail.com>
2026-04-19 17:48:46 +0200

    Set up Express server with CORS and JSON support

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 
+import express from "express";
+import cors from "cors";
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("IceDeals backend działa");
+});
+
+app.listen(4000, () => console.log("Backend running on port 4000"));
